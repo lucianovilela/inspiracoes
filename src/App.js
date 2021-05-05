16ade4a859a393e831bff6df5a1d4098de7eacf9
Luciano Vilela <lucianovilela@gmail.com>
2021-05-04 21:39:34 -0300

    versao com carrosel

--- src/App.js
+++ src/App.js
@@ -3,71 +3,115 @@ import React from "react";
 /* in ES 6 */
 import domtoimage from "dom-to-image";
 
-const MyCanvas = ({ list }) => {
-  const timer = React.useRef;
-  const [hora, setHora] = React.useState(new Date());
-  React.useEffect(() => {
-    timer.current = setInterval(() => {
-      setHora(new Date());
-    }, 1000);
-    return () => clearInterval(timer.current);
-  });
+const Carrocel = ({ list, setList }) => {
+  const onChange = (e, i) => {
+    list[i] = { ...list[i], valor: e.target.value };
 
+    setList({ ...list });
+  };
   return (
-    <div id="canvas-id" className="card">
-      <div className="card-header" style={{ marginTop: ".5em" }}>
+    <div id="carrocelId" className="card">
+      <div className="card-title">
         <h1>Minhas Inspirações</h1>
       </div>
       <div className="card-body">
-        {list.map((item) => (
-          <div className="row" style={{ margin: "0.5em" }} key={item.id}>
-            <div className="col-4">
-              <label className="form-label">{item.nome}</label>
-            </div>
-            <div className="col-8">
-              <input name="texto" type="text" className="form-control" />
-            </div>
+        <div
+          id="carouselExampleControls"
+          className="carousel slide carousel-fade"
+          data-bs-ride="false"
+        >
+          <div className="carousel-inner">
+            {Object.keys(list).map((i, index) => (
+              <div
+                className={`carousel-item ${index === 0 ? "active" : ""}`}
+                key={i}
+              >
+                <div>
+                  <label className="form-label">{list[i].nome}</label>
+                </div>
+                <div>
+                  <input
+                    name="texto"
+                    type="text"
+                    className="form-control"
+                    value={list[i].valor}
+                    onChange={(event) => onChange(event, i)}
+                  />
+                </div>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-      <div className="card-footer">
-        Gerado automaticamente com https://www.inspirational.tk
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleControls"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
-const Toast = () => (
-  <div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
-    <div className="toast-header">
-      <img src="..." className="rounded me-2" alt="..." />
-      <strong className="me-auto">Bootstrap</strong>
-      <small>11 mins ago</small>
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="toast"
-        aria-label="Close"
-      ></button>
+const MyCanvas = ({ list }) => {
+  return (
+    <div id="canvasId" className="card invisible">
+      <div className="card-title">
+        <h1>Minhas Inspirações</h1>
+      </div>
+      <div className="card-body">
+        {Object.keys(list).map((i) => (
+          <div className="row" style={{ margin: "0.5em" }} key={i}>
+            <div className="col-4">
+              <label className="form-label">{list[i].nome}</label>
+            </div>
+            <div className="col-8">
+              <div className="alert alert-primary" role="alert">
+                {list[i].valor}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="card-footer">
+        Gerado automaticamente com https://inspirational.tk
+      </div>
     </div>
-    <div className="toast-body">Hello, world! This is a toast message.</div>
-  </div>
-);
+  );
+};
 
 export default function App() {
   var node = React.useRef();
   const [img, setImg] = React.useState();
-  const [list, setList] = React.useState([
-    { id: 1, nome: "Guarda" },
-    { id: 2, nome: "Passagem" },
-    { id: 3, nome: "Pegada de costa" },
-    { id: 4, nome: "Queda" },
-    { id: 5, nome: "Raspagem" },
-    { id: 6, nome: "Finalização" },
-    { id: 7, nome: "Estratégia" }
-  ]);
+  const [list, setList] = React.useState({
+    "1": { nome: "Guarda", valor: "" },
+    "2": { nome: "Passagem", valor: "" },
+    "3": { nome: "Pegada de costa", valor: "" },
+    "4": { nome: "Queda", valor: "" },
+    "5": { nome: "Raspagem", valor: "" },
+    "6": { nome: "Finalização", valor: "" },
+    "7": { nome: "Estratégia", valor: "" }
+  });
   React.useEffect(() => {
-    node.current = document.getElementById("canvas-id");
+    node.current = document.getElementById("canvasId");
   });
 
   const click = () => {
@@ -91,6 +135,7 @@ export default function App() {
 
   return (
     <div className="App">
+      <Carrocel list={list} setList={setList} />
       <MyCanvas list={list} />
       <div>
         <button className="btn btn-primary" onClick={click}>
@@ -107,6 +152,7 @@ export default function App() {
           Download
         </a>
       </div>
+
       <div className="fixed-bottom">inspirational.tk</div>
     </div>
   );
