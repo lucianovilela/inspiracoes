620b1e576511cc0fe6c6df8481af9f9bcc284e86
Luciano Vilela <lucianovilela@gmail.com>
2021-05-06 15:57:09 -0300

    nova versao funcionado

---
+++ src/AlertDialog.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+export default function AlertDialog({ isOpen, title, text }) {
+  const [open, setOpen] = React.useState(isOpen ? true : false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {text}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/App.js
+++ src/App.js
@@ -3,105 +3,140 @@ import React from "react";
 /* in ES 6 */
 import domtoimage from "dom-to-image";
 
+import {
+  Button,
+  Card,
+  TextField,
+  InputLabel,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Link,
+  Paper
+} from "@material-ui/core";
+
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  Grid,
+  GridList
+} from "@material-ui/core";
+import Pagination from "@material-ui/lab/Pagination";
+
+import AlertDialog from "./AlertDialog";
 const Carrocel = ({ list, setList }) => {
-  const onChange = (e, i) => {
-    list[i] = { ...list[i], valor: e.target.value };
+  const [current, setCurrent] = React.useState({ id: 1, item: list[1] });
+
+  const onChange = (e) => {
+    current.item = { ...current.item, valor: e.target.value };
+    setCurrent({ ...current });
+    list[current.id] = { ...list[current.id], valor: e.target.value };
 
     setList({ ...list });
   };
   return (
-    <div id="carrocelId" className="card">
-      <div className="card-title">
-        <h1>Minhas Inspirações</h1>
-      </div>
-      <div className="card-body">
-        <div
-          id="carouselExampleControls"
-          className="carousel slide carousel-fade"
-          data-bs-ride="false"
-        >
-          <div className="carousel-inner">
-            {Object.keys(list).map((i, index) => (
-              <div
-                className={`carousel-item ${index === 0 ? "active" : ""}`}
-                key={i}
-              >
-                <div>
-                  <label className="form-label">{list[i].nome}</label>
-                </div>
-                <div>
-                  <input
-                    name="texto"
-                    type="text"
-                    className="form-control"
-                    value={list[i].valor}
-                    onChange={(event) => onChange(event, i)}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
-          <button
-            className="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="prev"
-          >
-            <span
-              className="carousel-control-prev-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Previous</span>
-          </button>
-          <button
-            className="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleControls"
-            data-bs-slide="next"
-          >
-            <span
-              className="carousel-control-next-icon"
-              aria-hidden="true"
-            ></span>
-            <span className="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-    </div>
+    <Card id="carrocelId" className="card">
+      <CardHeader title="Minhas Inspirações" />
+
+      <CardContent className="card-body">
+        <InputLabel className="form-label">{current.item.nome}</InputLabel>
+        <TextField
+          className="form-control"
+          onChange={(event) => onChange(event)}
+          value={current.item.valor}
+        />
+      </CardContent>
+      <CardActions>
+        <Grid container justify="center" xs={12}>
+          <Pagination
+            count={Object.keys(list).length}
+            variant="outlined"
+            onChange={(event, value) =>
+              setCurrent({ id: value, item: list[value] })
+            }
+            hideNextButton
+            hidePrevButton
+          />
+        </Grid>
+      </CardActions>
+    </Card>
   );
 };
 
-const MyCanvas = ({ list }) => {
+const MyCanvas = ({ list, show, setShow }) => {
+  const node = React.createRef();
+  const [img, setImg] = React.useState();
+  const click = () => {
+    console.log(node.current);
+    domtoimage
+      .toPng(node.current)
+      .then(async function (dataUrl) {
+        window.copyToClipboard(dataUrl);
+
+        setImg(dataUrl);
+      })
+      .catch(function (error) {
+        console.error("oops, something went wrong!", error);
+      });
+  };
   return (
-    <div id="canvasId" className="card invisible">
-      <div className="card-title">
-        <h1>Minhas Inspirações</h1>
-      </div>
-      <div className="card-body">
-        {Object.keys(list).map((i) => (
-          <div className="row" style={{ margin: "0.5em" }} key={i}>
-            <div className="col-4">
-              <label className="form-label">{list[i].nome}</label>
-            </div>
-            <div className="col-8">
-              <div className="alert alert-primary" role="alert">
-                {list[i].valor}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="card-footer">
-        Gerado automaticamente com https://inspirational.tk
-      </div>
+    <div>
+      <Dialog open={show} aria-labelledby="form-dialog-title">
+        <div id="canvasId" ref={node}>
+          <Card>
+            <CardHeader title="Minhas Inspirações" />
+            <CardContent>
+              <Grid container spacing={1}>
+                {Object.keys(list).map((i) => (
+                  <Grid container spacing={3} key={i}>
+                    <Grid item xs={4}>
+                      {list[i].nome}
+                    </Grid>
+
+                    <Grid item xs={8}>
+                      <Paper variant="outlined">{list[i].valor}&nbsp;</Paper>
+                    </Grid>
+                  </Grid>
+                ))}
+              </Grid>
+            </CardContent>
+            <CardActions>
+              Gerado automaticamente com https://inspirational.tk
+            </CardActions>
+          </Card>
+        </div>
+        <DialogActions>
+          <Button variant="outlined" color="primary" onClick={click}>
+            Copiar para Clipboard
+          </Button>
+          <Link
+            variant="outlined"
+            color="primary"
+            href={img}
+            style={{ display: img ? "" : "none", marginLeft: "1em" }}
+            target="_blank"
+            rel="noreferrer"
+            download
+          >
+            Download
+          </Link>
+          <Link
+            variant="outlined"
+            color="secondary"
+            onClick={() => setShow(false)}
+          >
+            Close
+          </Link>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 };
 
 export default function App() {
-  var node = React.useRef();
-  const [img, setImg] = React.useState();
-  const [list, setList] = React.useState({
+  const initial = {
     "1": { nome: "Guarda", valor: "" },
     "2": { nome: "Passagem", valor: "" },
     "3": { nome: "Pegada de costa", valor: "" },
@@ -109,50 +144,33 @@ export default function App() {
     "5": { nome: "Raspagem", valor: "" },
     "6": { nome: "Finalização", valor: "" },
     "7": { nome: "Estratégia", valor: "" }
-  });
-  React.useEffect(() => {
-    node.current = document.getElementById("canvasId");
-  });
-
-  const click = () => {
-    domtoimage
-      .toPng(node.current)
-      .then(async function (dataUrl) {
-        //let url = await window.saveArquivo(dataUrl);
-        window.copyToClipboard(dataUrl);
-
-        //console.log(url);
-        setImg(dataUrl);
-        //var img = new Image();
-        //img.src = dataUrl;
-        //document.body.appendChild(img);
-        alert("imagem copiada");
-      })
-      .catch(function (error) {
-        console.error("oops, something went wrong!", error);
-      });
   };
+  const [list, setList] = React.useState(initial);
+  const [showView, setShowView] = React.useState(false);
 
   return (
     <div className="App">
       <Carrocel list={list} setList={setList} />
-      <MyCanvas list={list} />
       <div>
-        <button className="btn btn-primary" onClick={click}>
-          Cópia para clipboard
-        </button>
-        <a
-          className="btn btn-primary"
-          href={img}
-          style={{ display: img ? "" : "none", marginLeft: "1em" }}
-          target="_blank"
-          rel="noreferrer"
-          download
+        <Button
+          variant="outlined"
+          color="primary"
+          style={{ margin: "0.5em" }}
+          onClick={() => setShowView(true)}
         >
-          Download
-        </a>
-      </div>
+          Visualizar
+        </Button>
 
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={() => setList(initial)}
+        >
+          Reset
+        </Button>
+      </div>
+      {/*<div>{JSON.stringify(list)}</div>*/}
+      {<MyCanvas list={list} show={showView} setShow={setShowView} />}
       <div className="fixed-bottom">inspirational.tk</div>
     </div>
   );
